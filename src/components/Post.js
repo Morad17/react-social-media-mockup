079800b66b47f079240fc4c7a9f367ebb441e986
Morad17 <morad123@hotmail.co.uk>
2021-10-28 23:25:12 +0100

    up

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,16 +1,42 @@
 import React from 'react'
 import '../assets/css/post.css'
+import Person1 from '../assets/images/person-1.jpg'
+import Post1 from '../assets/images/post-1.jpg'
+import Like from '../assets/images/like-icon.png'
+import Love from '../assets/images/love-icon.png'
+
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 const Post = () => {
     return (
         <div className="post-container">
             <div className="post-wrapper">
                 <div className="post-top">
-                    <div className="post-top-left"></div>
-                    <div className="post-top-right"></div>
+                    <div className="post-top-left">
+                        <img src={ Person1 } alt="" className="post-profile-image" />
+                        <span className="post-username">Morad Elb</span>
+                        <span className="post-date">5 minutes ago</span>
+                    </div>
+                    <div className="post-top-right">
+                        <MoreVertIcon className="" />
+                    </div>
                 </div>
-                <div className="post-center"></div>
-                <div className="post-bottom"></div>
+                <div className="post-center">
+                    <span className="post-text">
+                        Hello there World
+                    </span>
+                    <img src={ Post1 } alt="" className="post-image" />
+                </div>
+                <div className="post-bottom">
+                    <div className="post-bottom-left">
+                        <img src={ Like } alt="" className="like-icon" />
+                        <img src={ Love } alt="" className="like-icon" />
+                        <span className="post-like-counter">32 likes</span>
+                    </div>
+                    <div className="post-bottom-right">
+                        <span className="post-comment-text">9 comments</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import '../assets/css/rightbar.css'
 
+import Birthday from '../assets/images/birthday-icon.png'
+import Ad from '../assets/images/stock-ad.jpg'
+import Person2 from '../assets/images/person-2.jpg'
+
 const Rightbar = () => {
     return (
         <div className="rightbar-container">
-            rightbar
+            <div className="rightbar-wrapper">
+                <div className="birthday-container">
+                    <img src={ Birthday } alt="" className="birthday-image" />
+                    <span className="birthday-text">3 Freinds have a birthday today</span>
+                </div>
+                <img src={ Ad } alt="" className="rightbar-ad" />
+                <h4 className="rightbat-title"></h4>
+                <ul className="rightbar-friendlist">
+                    <li className="rightbar-friend">
+                        <div className="rightbar-profile-image-container">
+                            <img src={ Person2 } alt="" className="rightbar-profile-image" />
+                            <span className="rightbar-online"></span>
+                        </div>
+                        <span className="rightbar-username">John Doe</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
