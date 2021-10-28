da778ea78e2c1cf3cd04c693e73ad972e38b5320
Morad17 <morad123@hotmail.co.uk>
2021-10-28 20:06:27 +0100

    createing feed and homepage

---
+++ src/components/Post.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../assets/css/post.css'
+
+const Post = () => {
+    return (
+        <div className="post-container">
+            <div className="post-wrapper">
+                <div className="post-top">
+                    <div className="post-top-left"></div>
+                    <div className="post-top-right"></div>
+                </div>
+                <div className="post-center"></div>
+                <div className="post-bottom"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Post
